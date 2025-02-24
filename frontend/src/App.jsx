a23821e1f60a7178349cc01038ebce69b101acc3
Stephanie <stephnjonjo@gmail.com>
2025-02-24 22:21:46 +0300

    defined the app component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,13 @@
 import {BrowserRouter as Router, Route, Routes} from react-router-dom;
 import Login from './components/Login';
 import Signup from './components/Signup';
+
+function App() {
+  <Router>
+    <h1>FlashLearn</h1>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
+  </Router>
+}
