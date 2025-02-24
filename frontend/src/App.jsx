d2f50eb99ecf8b289c9670bdff17aa730ca5317d
Stephanie <stephnjonjo@gmail.com>
2025-02-24 22:42:34 +0300

    edited some files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,16 @@
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import Login from './components/Login';
-import Signup from './components/Signup';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
 
 function App() {
-  <Router>
-    <h1>FlashLearn</h1>
-    <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-    </Routes>
-  </Router>
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
