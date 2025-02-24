78c2dc7e63e2776ac2e68f46538c8667be84c238
Stephanie <stephnjonjo@gmail.com>
2025-02-24 20:58:16 +0300

    set up state variables for email, password and err message

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,4 +1,8 @@
 import {useState} from 'react';
 import axios from 'axios';
 
-const Login = () => {}
+const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+}
