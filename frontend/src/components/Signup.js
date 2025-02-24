373473c864f5d6348c86616c3e38a4a7adcef536
Stephanie <stephnjonjo@gmail.com>
2025-02-24 21:55:35 +0300

    created the handlesignup func

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -5,4 +5,6 @@ const Signup = () => {
     const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+
+    const handleSignup = async (e) => {
 }
