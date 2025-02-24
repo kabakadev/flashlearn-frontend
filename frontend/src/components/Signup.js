3fdb193d370b8949337a4661bcb856f7dbdac697
Stephanie <stephnjonjo@gmail.com>
2025-02-24 21:54:27 +0300

    set up the state variables for username, email and password

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,4 +1,8 @@
 import { useState } from 'react';
 import axios from 'axios'; 
 
-const Signup = () => {}
+const Signup = () => {
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+}
