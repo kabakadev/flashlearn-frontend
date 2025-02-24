3420edd643746d0357cee154153265c35ebfb4b3
Stephanie <stephnjonjo@gmail.com>
2025-02-24 22:26:49 +0300

    added quotation marks when importing react router dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Route, Routes} from react-router-dom;
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Login from './components/Login';
 import Signup from './components/Signup';
 
