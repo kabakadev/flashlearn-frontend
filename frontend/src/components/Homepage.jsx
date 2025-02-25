badf07faf6a1e52223d58706996dd1737b357abf
Stephanie <stephnjonjo@gmail.com>
2025-02-25 18:20:59 +0300

    added navigation links and exported homepage

--- frontend/src/components/Homepage.jsx
+++ frontend/src/components/Homepage.jsx
@@ -1,1 +1,20 @@
 import { Link } from "react-router-dom";
+
+const Homepage = (){
+    return(
+        <div>
+           <h1> Welcomee to FlashLearnnn!!!!!</h1>
+           <p> Your place to master flashcardss.</p>
+
+           <Link to ="/login">
+           <button>Login</button>
+           </Link>
+
+           <Link to ="/register">
+           <button>Register</button>
+           </Link>
+        </div>
+    );
+};
+
+export default Homepage;
