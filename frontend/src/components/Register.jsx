e48e6a687bebe2b7f97f327b9b882341e7a81a8e
Stephanie <stephnjonjo@gmail.com>
2025-02-25 18:14:23 +0300

    added login link

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -64,6 +64,9 @@ const Register = () => {
         />
         <button type="submit">Register</button>
       </form>
+      <p>
+        Already have an account?? <Link to "/login">Login here</Link>
+      </p>
     </div>
   );
 };
