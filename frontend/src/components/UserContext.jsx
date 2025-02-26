e6f75f74155eac99c46dc94b2746814da1db38fc
Stephanie <stephnjonjo@gmail.com>
2025-02-26 12:31:10 +0300

    usestate to track user authentication

--- frontend/src/components/UserContext.jsx
+++ frontend/src/components/UserContext.jsx
@@ -3,3 +3,4 @@ import { createContext, useContext, useState, useEffect } from "react";
 const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {}
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
